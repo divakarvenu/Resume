20814e79a6ba123a3b5de0d0c8ac8def01eac72f
vbinithyanandamv <nithyanandamv@visualbi.com>
2020-05-28 14:26:54 +0530

    Initial commit

---
+++ Webpack/webpack.common.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack'); // to access built-in plugins
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contentHash].bundle.js'
+  },
+  module: {
+    rules: [
+            {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ]
+};
---
+++ Webpack/webpack.prod.js
@@ -0,0 +1,13 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    "mode": "production",
+    plugins:[ 
+        new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "index.[hash].html"
+      })
+    ]
+});
